12a314cecd2cc6629fab24885e9a018f321fecb1
Damien Flury <damien.flury@gmail.com>
2020-03-05 20:02:05 +0100

    Add styled-components

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,8 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx", ".tsx"] }],
     'import/no-unresolved': 'off',
-    'import/extensions': 'off'
+    'import/extensions': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
   },
 };
