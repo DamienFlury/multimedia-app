a645f8f0eb13a75d5f523728ed5f25dbd3f47b4a
Damien Flury <damien.flury@gmail.com>
2020-03-05 16:20:53 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx", ".tsx"] }],
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off'
+  },
+};
