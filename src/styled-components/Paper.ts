be617d1f73125d623bcd81d45139751db2dbb691
damienflury <damienflury@pop-os.localdomain>
2020-03-07 12:05:58 +0100

    Remove margin and padding from paper

--- src/styled-components/Paper.ts
+++ src/styled-components/Paper.ts
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 const Paper = styled.div`
   background: ${props => props.theme.colors.background[1]};
   box-shadow: ${props => props.theme.shadows[0]};
-  padding: 20px;
-  margin: 20px;
   border-radius: ${props => props.theme.borderRadius};
   transition: 0.3s;
   color: ${props => props.theme.colors.text};
